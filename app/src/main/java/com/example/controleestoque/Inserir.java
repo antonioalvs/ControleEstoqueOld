9b42b322db069560241e3b28e2cbc451932f1994
antonioalvs <antonio.aa.alvs@gmail.com>
2023-01-24 23:08:25 -0300

    Tela Inserir ok

--- app/src/main/java/com/example/controleestoque/Inserir.java
+++ app/src/main/java/com/example/controleestoque/Inserir.java
@@ -1,13 +1,73 @@
 package com.example.controleestoque;
 
+import model.Produto;
+//import model.No;
+import model.ListaEncadeada;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Inserir extends AppCompatActivity {
+
+
+    Button prx, save;
+    EditText nome, valor, qtdAtual, qtdMin;
+    TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir);
+
+        //Definindo campos e botões
+        prx = findViewById(R.id.btnProximo);
+        save = findViewById(R.id.btnSalvar);
+        Produto p = new Produto();
+        ListaEncadeada le = new ListaEncadeada();
+        prx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Variáveis dos campos
+                nome = findViewById(R.id.txtNome);
+                valor = findViewById(R.id.txtValor);
+                qtdAtual = findViewById(R.id.txtQtdAtual);
+                qtdMin = findViewById(R.id.txtQtdMin);
+                res = findViewById(R.id.lblResult);
+                //Variaveis comuns
+                String n; //nome
+                float v; //valor
+                int qta, qtm; // qtd atual e qtd minima
+
+
+
+                //Passando valor dos campos para variáveis
+                n = String.valueOf(nome.getText());
+                v = Float.parseFloat(String.valueOf(valor.getText()));
+                qta = Integer.parseInt(String.valueOf(qtdAtual.getText()));
+                qtm = Integer.parseInt(String.valueOf(qtdMin.getText()));
+
+                //Construindo o Produto
+                p.setCodigo(0);
+                p.setNomeProduto(n);
+                p.setValorProduto(v);
+                p.setQtdAtual(qta);
+                p.setQtdMinima(qtm);
+
+                //Inserindo na lista
+                le.insert(p);
+                res.setText(le.imprimirLista());
+
+                //Limpa campos
+                nome.setText("");
+                valor.setText("");
+                qtdAtual.setText("");
+                qtdMin.setText("");
+
+            }
+        });
     }
 }
--- app/src/main/java/model/ListaEncadeada.java
+++ app/src/main/java/model/ListaEncadeada.java
@@ -38,4 +38,62 @@ public class ListaEncadeada {
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
+
+    //Funções
+
+    //Verifica se a lista está vazia
+    public boolean listaVazia(){ //Verifica se a lista está vazia
+        return(this.primeiro==null);
+    }
+
+    //Insere o produto na lista
+    public void insert(Produto p) {
+        No novoNo = new No(p);  //instancia um novo nó
+        if (this.listaVazia()) {
+            this.ultimo = novoNo;
+        }
+        novoNo.setProximo(this.primeiro);
+        this.primeiro=novoNo;
+        this.quantidade++;
+
+
+
+    }
+
+    //Imprime a Lista
+    public String imprimirLista(){
+        String msg="", reporProd;
+        int i=1;
+
+        No atual=this.primeiro;
+        while(atual!=null){
+            if(atual.getProd().getQtdAtual()<atual.getProd().getQtdMinima()){
+                reporProd="REPOR ESTOQUE DO PRODUTO";
+            }
+            else{
+                reporProd="";
+            }
+            msg+= +i+"."+atual.getProd().getNomeProduto()+"\t |PRECO: R$"+atual.getProd().getValorProduto()+
+                    "\t |QTD ATUAL: "+atual.getProd().getQtdAtual()+"\t |QTD MINIMA: "+atual.getProd().getQtdMinima()+"\t|"+reporProd+"\n";
+            atual=atual.getProximo();
+            i++;
+        }
+
+        return msg;
+    }
+
+    public String sqlite(){
+        String msg="", reporProd;
+        int i=1;
+
+        No atual=this.primeiro;
+        while(atual!=null){
+            msg+= +i+"."+atual.getProd().getNomeProduto()+"\t |PRECO: R$"+atual.getProd().getValorProduto()+
+                    "\t |QTD ATUAL: "+atual.getProd().getQtdAtual()+"\t |QTD MINIMA: "+atual.getProd().getQtdMinima();
+            atual=atual.getProximo();
+            i++;
+        }
+
+        return msg;
+    }
 }
